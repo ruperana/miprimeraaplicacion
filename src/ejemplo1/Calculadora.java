696e84f70395748cd42676eb4e603d161788586c
Manuela <Manuela@DESKTOP-5NCS782>
2021-04-22 17:25:40 +0200

    Para rama calculadora

---
+++ src/ejemplo1/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author Manuela
+ */
+public class Calculadora {
+    public int suma( int a, int b)
+    {
+        return a +b ;
+    }
+    
+}
